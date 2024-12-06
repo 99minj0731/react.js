3bede2e9306b6ecdebf38dfd9d125c6a49796510
99minj0731 <99minj0731@naver.com>
2024-12-06 22:28:16 +0900

    24.12.06

---
+++ section05/src/components/Header.jsx
@@ -0,0 +1,19 @@
+// 함수로 component 생성 , 첫번째 글자는 반드시 대문자로 작성
+function Header() {
+  return (
+    <header>
+      <h1>header</h1>
+    </header>
+  )
+}
+
+export default Header
+
+// 화살표 함수로 component 생성
+// const Header = () => {
+//   return (
+//     <header>
+//       <h1>header</h1>
+//     </header>
+//   )
+// }
