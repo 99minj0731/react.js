da097f3de0499564befea4b5f98f553f5f1f6581
99minj0731 <99minj0731@naver.com>
2024-12-10 17:05:21 +0900

    2024.12.10

---
+++ TodoList_02/src/App.jsx
@@ -0,0 +1,17 @@
+import './App.css'
+import Header from './component/Header'
+import Editor from './component/Editor'
+import List from './component/List'
+
+function App() {
+
+  return (
+    <>
+      <Header />
+      <Editor />
+      <List />
+    </>
+  )
+}
+
+export default App
