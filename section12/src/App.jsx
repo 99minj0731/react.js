d04c268a12ad038d2b14ecfc5f451e121b2913da
99minj0731 <99minj0731@naver.com>
2024-12-22 00:22:45 +0900

    DiaryFin

--- section12/src/App.jsx
+++ section12/src/App.jsx
@@ -1,7 +1,7 @@
 // App.jsx
 
 import './App.css'
-import { useReducer, useRef, createContext} from 'react'
+import { useReducer, useRef, createContext, useEffect, useState} from 'react'
 import { Routes, Route } from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
@@ -13,41 +13,35 @@ import NotFound from './pages/NotFound'
 // 2. "/new" : 새로운 일기를 작성하는 New 페이지
 // 3. "/diary" : 일기를 상세히 조회하는 Diary 페이지
 
-const mockData = [
-  {
-    id :1, 
-    createdDate: new Date("2024-12-19").getTime(),
-    emotionId: 1, 
-    content:" 1번 일기 내용",
-  },
-  {
-    id :2, 
-    createdDate: new Date("2024-12-18").getTime(),
-    emotionId: 2, 
-    content:" 2번 일기 내용",
-  },
-  {
-    id :3, 
-    createdDate: new Date("2024-11-12").getTime(),
-    emotionId: 3, 
-    content:" 3번 일기 내용",
-  }
-]
-
 function reducer(state, action) {
+  let nextState 
+
   switch(action.type) {
+    case 'INIT': 
+      return action.data
     case 'CREATE' :
-      return [action.data, ...state]
+      { 
+        nextState= [action.data, ...state]
+        break
+      }
     case 'UPDATE' :
-      return state.map((item)=> 
+      {
+        nextState = state.map((item)=> 
         String(item.id) === String(action.data.id) 
       ? action.data 
       : item )
+      break
+    }
     case 'DELETE' :
-      return state.filter((item) => String(item.id) !== String(action.id))
+      {
+        nextState = state.filter((item) => String(item.id) !== String(action.id))
+        break
+      }
     default:
       return state
   }
+  localStorage.setItem("diary", JSON.stringify(nextState))
+  return nextState
 }
 
 export const DiaryStateContext = createContext()
@@ -55,8 +49,40 @@ export const DiaryDispatchContext = createContext()
 
 function App() {
 
-  const [data, dispatch] = useReducer(reducer, mockData )
-  const idRef = useRef(3)
+  const [isLoading, setIsLoading] = useState(true)
+
+  const [data, dispatch] = useReducer(reducer, [] )
+  const idRef = useRef(0)
+
+  useEffect(()=>{
+    const storedData = localStorage.getItem("diary")
+    if(!storedData){
+      setIsLoading(false)
+      return
+    }
+    const parsedData = JSON.parse(storedData)
+    //true, false를 반환 
+    if(!Array.isArray(parsedData)){
+      setIsLoading(false)
+      return 
+    }
+
+    let maxId = 0
+    parsedData.forEach((item) => {
+      if(Number(item.id) > maxId){
+        maxId = Number(item.id)
+      }
+    })
+
+    // console.log(maxId)
+    idRef.current = maxId + 1
+
+    dispatch({
+      type:"INIT",
+      data:parsedData
+    })
+    setIsLoading(false)
+  }, [])
 
   // 새로운 일기 추가
   const onCreate =  (createdDate, emotionId, content ) => {
@@ -95,6 +121,11 @@ function App() {
       }
     )
   }
+
+  if(isLoading) {
+    return <div>데이터 로딩중입니다... </div>
+  }
+
   return (
     <>
       <DiaryStateContext.Provider value={data}>
--- section12/src/components/Editor.jsx
+++ section12/src/components/Editor.jsx
@@ -1,49 +1,12 @@
 import "./Editor.css"
 import EmotionItem from "./EmotionItem"
 import Button from "./Button"
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { useNavigate } from "react-router-dom"
+import { emotionList } from "../util/constants"
+import { getStringedDate } from "../util/get-stringed-date"
 
-const emotionList = [
-  {
-    emotionId:1,
-    emotionName:"완전 좋음"
-  },
-  {
-    emotionId:2,
-    emotionName:"좋음"
-  },
-  {
-    emotionId:3,
-    emotionName:"그럭저럭"
-  },
-  {
-    emotionId:4,
-    emotionName:"나쁨"
-  },
-  {
-    emotionId:5,
-    emotionName:"끔찍한"
-  },
-]
-
-const getStringedDate = (targetDate) => {
-  //날짜 -> YYYY-MM-DD
-  let year = targetDate.getFullYear()
-  let month = targetDate.getMonth() +1
-  let date = targetDate.getDate()
-
-  if(month < 10){
-    month = `0${month}`
-  }
-  if(date < 10){
-    date = `0${date}`
-  }
-
-  return `${year}-${month}-${date}`
-}
-
-const Editor = ({onSubmit}) => {
+const Editor = ({onSubmit, initData}) => {
 
   
   const [input, setInput] = useState({
@@ -54,9 +17,18 @@ const Editor = ({onSubmit}) => {
 
   const nav = useNavigate()
   
+  useEffect(()=>{
+    if(initData){
+      setInput({
+        ...initData,
+        createdDate: new Date(Number(initData.createdDate))
+      })
+    }
+  }, [initData])
+
   const onChangeInput = (e)=>{
-    console.log(e.target.name) // 어떤 요소에 입력이 들어온건지 확인
-    console.log(e.target.value) // 입력된 값이 무엇인지
+    // console.log(e.target.name) // 어떤 요소에 입력이 들어온건지 확인
+    // console.log(e.target.value) // 입력된 값이 무엇인지
 
     let name = e.target.name
     let value = e.target.value 
