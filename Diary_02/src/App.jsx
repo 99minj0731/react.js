61eb9bda22ad0ffe46496b91f2c0e2b4bee56a99
99minj0731 <99minj0731@naver.com>
2024-12-26 22:32:08 +0900

    create

--- Diary_02/src/App.jsx
+++ Diary_02/src/App.jsx
@@ -4,30 +4,101 @@ import Home from './pages/Home'
 import Edit from './pages/Edit'
 import New from './pages/New'
 import NotFound from './pages/NotFound'
-import Button from './components/Button'
 
-import { getEmotionImage } from './util/get-emotion-image'
+import { Route, Routes } from 'react-router-dom'
+import { useReducer, useRef, createContext } from 'react'
 
-import { Route, Routes, Link, useNavigate } from 'react-router-dom'
+const mockData = [
+  {
+    id:1, 
+    createdDate: new Date("2024-12-26").getTime(),
+    emotionId: 1,
+    content: "1번 일기내용",
+  },
+  {
+    id:2, 
+    createdDate: new Date("2024-12-18").getTime(),
+    emotionId: 2,
+    content: "2번 일기내용",
+  },
+  {
+    id:3, 
+    createdDate: new Date("2024-11-18").getTime(),
+    emotionId: 3,
+    content: "3번 일기내용",
+  },
+]
+
+function reducer(state, action) {
+  switch(action.type){
+    case 'CREATE':
+      return[action.data, ...state]
+      case 'UPDATE':
+        return state.map((item) => 
+          String(item.id) === String(action.data.id)
+        ? action.data
+        :item 
+      )
+      case 'DELETE' :
+        return state.filter((item)=> 
+          String(item.id) !== String(action.id))
+        default:
+          return state
+    }
+  }
+
+  export const DiaryStateContext = createContext()
+  export const DiaryDispatchContext = createContext()
 
 function App() {
 
-  const nav = useNavigate()
+  const [data, dispatch] = useReducer(reducer, mockData)
+  const idRef = useRef(3)
 
-  const onclickButton = () => {
-    nav('/new')
+
+  const onCreate = (createdDate, emotionId, content) => {
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate, 
+        emotionId, 
+        content,
+      }
+    })
   }
 
+  const onUpdate = (id, createdDate, emotionId, content) => {
+    dispatch({
+      type: "UPDATE",
+      data: {
+        id,
+        createdDate, 
+        emotionId,
+        content,
+      }
+    })
+  }
+
+  const onDelete = (id) => {
+    dispatch({
+      type: "DELETE",
+      id,
+    })
+  }
   return (
     <>
-      <Button />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/new' element={<New />} />
-        <Route path='/diary/:id' element={<Diary />} />
-        <Route path='/edit/:id' element={<Edit />} />
-        <Route path='*' element={<NotFound />} />
-      </Routes>
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{onCreate, onUpdate, onDelete}}>
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/new' element={<New />} />
+            <Route path='/diary/:id' element={<Diary />} />
+            <Route path='/edit/:id' element={<Edit />} />
+            <Route path='*' element={<NotFound />} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
--- Diary_02/src/pages/Home.jsx
+++ Diary_02/src/pages/Home.jsx
@@ -1,5 +1,41 @@
+import Header from "../components/Header"
+import Button from "../components/Button"
+import DiaryList from "../components/DiaryList"
+
+
+import { useState, useContext } from "react"
+import { DiaryStateContext } from "../App"
+
+
 const Home = () => {
-  return <div>Home</div>
+
+  const data = useContext(DiaryStateContext)
+
+  const [pivotDate, setPivotDate] = useState(new Date())
+
+  const getMonthlyData = (pivotDate, data) => {
+    const beginTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth(), 1, 0, 0, 0).getTime()
+    const endTime = new Date(pivotDate.getFullYear(), pivotDate.getMonth() +1, 0, 23, 59, 59).getTime()
+    return data.filter((item) => beginTime <= item.createdDate && item.createdDate <= endTime )
+  }
+
+  const monthlyData = getMonthlyData(pivotDate, data)
+
+  const onIncreasedMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() + 1))
+  }
+  const onDecreasedMonth = () => {
+    setPivotDate(new Date(pivotDate.getFullYear(), pivotDate.getMonth() -1 ))
+  }
+
+  return <div>
+    <Header 
+      title={`${pivotDate.getFullYear()}년 ${pivotDate.getMonth() + 1}월`}
+      leftChild={<Button onClick={onDecreasedMonth} text={"<"}/>}  
+      rightChild={<Button onClick={onIncreasedMonth} text={">"}/>}
+    />
+    <DiaryList data={monthlyData} />
+  </div>
 }
 
 export default Home
